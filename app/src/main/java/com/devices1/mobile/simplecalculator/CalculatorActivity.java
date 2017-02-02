78c454ae6c491bd91783c73121f79998a44640a0
Borisgg <basurabobbs@gmail.com>
2017-02-02 16:42:01 +0100

    primer cambio

--- app/src/main/java/com/devices1/mobile/simplecalculator/CalculatorActivity.java
+++ app/src/main/java/com/devices1/mobile/simplecalculator/CalculatorActivity.java
@@ -8,6 +8,7 @@ public class CalculatorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //puta
         setContentView(R.layout.activity_calculator);
     }
 }
